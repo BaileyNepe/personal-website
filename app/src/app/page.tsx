d68fc6138c0c6dc62b95fe8f04f0e769f4b5c496
Bailey Nepe <bailey.nepe@live.com>
2025-07-18 19:03:30 +1200

    add avatar image

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,12 +5,15 @@ import Link from 'next/link'
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
+import { ArrowDownIcon } from '@/components/Icons/ArrowDownIcon'
+import { BriefcaseIcon } from '@/components/Icons/BriefcaseIcon'
 import {
   GitHubIcon,
   InstagramIcon,
   LinkedInIcon,
   XIcon,
 } from '@/components/Icons/SocialIcons'
+import avatarImage from '@/images/avatar.jpg'
 import logoAirbnb from '@/images/logos/airbnb.svg'
 import logoFacebook from '@/images/logos/facebook.svg'
 import logoPlanetaria from '@/images/logos/planetaria.svg'
@@ -22,65 +25,6 @@ import image4 from '@/images/photos/image-4.jpg'
 import image5 from '@/images/photos/image-5.jpg'
 import { type ArticleWithSlug, getAllArticles } from '@/lib/articles'
 import { formatDate } from '@/lib/formatDate'
-
-function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function BriefcaseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function ArrowDownIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
 
 function Article({ article }: { article: ArticleWithSlug }) {
   return (
@@ -109,7 +53,6 @@ function SocialLink({
 }
 
 function Education() {
-
   const roles: Role[] = [
     {
       company: 'University of California, Berkeley',
@@ -127,8 +70,8 @@ function Education() {
     },
   ]
   return (
-   <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
-    <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
         <span className="ml-3">Education</span>
       </h2>
@@ -137,7 +80,7 @@ function Education() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-   </div>
+    </div>
   )
 }
 
@@ -235,53 +178,47 @@ function Resume() {
   )
 }
 
-function Photos() {
-  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
-
-  return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              'relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-              rotations[imageIndex % rotations.length],
-            )}
-          >
-            <Image
-              src={image}
-              alt=""
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
-
 export default async function Home() {
   let articles = (await getAllArticles()).slice(0, 4)
 
   return (
     <>
-      <Container className="mt-24">
-        <div className="max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
-          </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York City. I’m
-            the founder and CEO of Planetaria, where we develop technologies that empower
-            regular people to explore space on their own terms.
-          </p>
-          <div className="mt-6 flex gap-6">
-            <SocialLink href="#" aria-label="Follow on X" icon={XIcon} />
-            <SocialLink href="#" aria-label="Follow on Instagram" icon={InstagramIcon} />
-            <SocialLink href="#" aria-label="Follow on GitHub" icon={GitHubIcon} />
-            <SocialLink href="#" aria-label="Follow on LinkedIn" icon={LinkedInIcon} />
+      <Container className="mt-12 md:mt-24">
+        <div className="max-w-2xl lg:max-w-5xl">
+          <div className="flex flex-col-reverse gap-6 sm:flex-row sm:items-center sm:justify-between">
+            <div className="flex-1 md:max-w-8/12">
+              <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+                Software designer, founder, and amateur astronaut.
+              </h1>
+              <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                I&apos;m Spencer, a software designer and entrepreneur based in New York
+                City. I&apos;m the founder and CEO of Planetaria, where we develop
+                technologies that empower regular people to explore space on their own
+                terms.
+              </p>
+              <div className="mt-6 flex gap-6">
+                <SocialLink href="#" aria-label="Follow on X" icon={XIcon} />
+                <SocialLink
+                  href="#"
+                  aria-label="Follow on Instagram"
+                  icon={InstagramIcon}
+                />
+                <SocialLink href="#" aria-label="Follow on GitHub" icon={GitHubIcon} />
+                <SocialLink
+                  href="#"
+                  aria-label="Follow on LinkedIn"
+                  icon={LinkedInIcon}
+                />
+              </div>
+            </div>
+            <div className="flex flex-shrink-0">
+              <Image
+                src={avatarImage}
+                alt="Avatar"
+                className="h-24 w-24 rounded-full object-cover sm:h-32 sm:w-32 lg:h-36 lg:w-36"
+                priority
+              />
+            </div>
           </div>
         </div>
       </Container>
