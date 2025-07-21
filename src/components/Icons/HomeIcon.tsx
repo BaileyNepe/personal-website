35e35af9255375bb10b94d580916174595ccf8bd
Bailey Nepe <bailey.nepe@live.com>
2025-07-22 07:51:16 +1200

    style improvements

--- src/components/Icons/HomeIcon.tsx
+++ src/components/Icons/HomeIcon.tsx
@@ -1,12 +1,13 @@
 export function HomeIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+    <svg viewBox='0 0 24 24' aria-hidden='true' {...props}>
       <path
-        d="M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        d='M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z'
+        fill='none'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   )
-} 
+}
